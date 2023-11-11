ca07ede0f00b6ca557504f9dc92d21e2c04bba7f
Laszlo Bombolya <laszlo.bombolya@codecool.com>
2023-11-11 14:54:26 +0100

    Add startercode

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "src/Game.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    Game game;
+    return 0;
+}
---
+++ src/Game.h
@@ -0,0 +1,13 @@
+#ifndef CPP_FIVE_IN_A_ROW_GAME_H
+#define CPP_FIVE_IN_A_ROW_GAME_H
+
+using namespace std;
+class Game {
+private:
+
+public:
+
+};
+
+
+#endif //CPP_FIVE_IN_A_ROW_GAME_H
